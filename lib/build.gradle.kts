1034de870c711076434de40ccd5f35ab2d56830d
Jack Lingle <captainjack0818@gmail.com>
2024-04-16 18:29:02 -0400

    add utils folder from 2024-robot and add WPILib dependencies + plugin

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,7 +7,9 @@
 
 plugins {
     // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+    id `java-library`
+    // WPILib plugin
+    id `edu.wpi.first.GradleRIO` version `2024.3.2`
 }
 
 repositories {
@@ -26,6 +28,10 @@ dependencies {
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation(libs.guava)
+
+    // WPILib dependencies
+    implementation wpi.java.deps.wpilib()
+	implementation wpi.java.vendor.java()
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
