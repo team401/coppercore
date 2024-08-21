aaef2ce6a081b5dd03e90b793a10969b762554cd
aidnem <99768676+aidnem@users.noreply.github.com>
2024-08-20 21:09:01 -0400

    Fix build errors and enable publishing to build jar artifacts.

---
+++ wpi_interface/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "com.github.team401";
+    public static final String MAVEN_NAME = "wpi_interface";
+    public static final String VERSION = "0.0";
+    public static final int GIT_REVISION = 14;
+    public static final String GIT_SHA = "18aeea1d8ec130e803b61501a67f53881c126435";
+    public static final String GIT_DATE = "2024-08-19 19:21:49 EDT";
+    public static final String GIT_BRANCH = "9-enable-publishing";
+    public static final String BUILD_DATE = "2024-08-20 21:08:15 EDT";
+    public static final long BUILD_UNIX_TIME = 1724202495398L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
