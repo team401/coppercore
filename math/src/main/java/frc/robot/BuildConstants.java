42b80a5729a8f4bb3432065f581570419890e699
minhnguyenbhs <minhnguyen@mcps.org>
2024-08-28 19:37:25 -0400

    ??? unsure what is broken, currently copying in entire wpilibinterface gradle to math

---
+++ math/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "com.github.team401";
+    public static final String MAVEN_NAME = "math";
+    public static final String VERSION = "0.0";
+    public static final int GIT_REVISION = 28;
+    public static final String GIT_SHA = "720e2fefa36fbad886a9fb43adb86569af695bf0";
+    public static final String GIT_DATE = "2024-08-28 19:23:06 EDT";
+    public static final String GIT_BRANCH = "gradlefix";
+    public static final String BUILD_DATE = "2024-08-28 19:37:08 EDT";
+    public static final long BUILD_UNIX_TIME = 1724888228775L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
