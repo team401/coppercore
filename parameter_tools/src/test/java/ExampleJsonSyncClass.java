850e4340af8d2fd4eaab499aef85254f3364c96f
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2024-09-09 19:06:37 -0400

    Add json config (#18)
    * Added ability to set file its unit test
    
    * Prepare to merge with main
    
    * Disable Tests
    
    * Added Spotless
    
    * Actual Added Spotless

---
+++ parameter_tools/src/test/java/ExampleJsonSyncClass.java
@@ -0,0 +1,43 @@
+package coppercore.paremeter_tools.test;
+
+import coppercore.parameter_tools.JSONSync;
+
+public class ExampleJsonSyncClass {
+
+    public static JSONSync<ExampleJsonSyncClass> synced =
+            new JSONSync<ExampleJsonSyncClass>(
+                    new ExampleJsonSyncClass(),
+                    "filePath",
+                    new JSONSync.JSONSyncConfigBuilder().build());
+
+    public final String testText = "";
+    public final Double testDouble = 0.0;
+    public final Integer testInt = 0;
+    public final BasicMotorDataHolder motorData = null;
+
+    public class BasicMotorDataHolder {
+        public final Double maxVoltage = 0.0;
+        public final Double minVoltage = 0.0;
+        public Double currentVoltage = 0.0;
+
+        public String toString() {
+            return "minVoltage: "
+                    + minVoltage
+                    + "\nmaxVoltage: "
+                    + maxVoltage
+                    + "\ncurrentVoltage: "
+                    + currentVoltage;
+        }
+    }
+
+    public String toString() {
+        return "testText: "
+                + testText
+                + "\ntestInt: "
+                + testInt
+                + "\ntestDouble: "
+                + testDouble
+                + "\nmotorData: "
+                + motorData;
+    }
+}
