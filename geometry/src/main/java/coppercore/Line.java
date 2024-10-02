f902e77318e26bb6c802c9ed5d0760edf58c6167
Jack Lingle <captainjack0818@gmail.com>
2024-10-02 18:57:52 -0400

    remove slope variable

--- geometry/src/main/java/coppercore/Line.java
+++ geometry/src/main/java/coppercore/Line.java
@@ -5,14 +5,12 @@ public class Line {
     private double y1;
     private double x2;
     private double y2;
-    private double slope;
 
     public Line(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        this.slope = (y2 - y1) / (x2 - x1);
     }
 
     public Line(Point point1, Point point2) {
@@ -20,7 +18,6 @@ public class Line {
         this.y1 = point1.getY();
         this.x2 = point2.getX();
         this.y2 = point2.getY();
-        this.slope = (this.y2 - this.y1) / (this.x2 - this.x1);
     }
 
     public boolean isPointOnLine(Point point) {
