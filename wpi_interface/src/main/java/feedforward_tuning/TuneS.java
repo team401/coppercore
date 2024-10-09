136140185cfbf1ec113eb91013adf300964688c9
aidnem <99768676+aidnem@users.noreply.github.com>
2024-10-09 16:38:51 +0000

    Add TuneG, TuneS, and TuneV to wpi_interface.feedforward_tuning
    Also add coppercore.controls as a dependency of wpi_interface.
    UNTESTED - This code needs to be built and tested before it is used,
    as it wouldn't build properly in GitHub codespaces.

---
+++ wpi_interface/src/main/java/feedforward_tuning/TuneS.java
@@ -0,0 +1,47 @@
+// TODO: WIP - Not tested
+
+package coppercore.wpi_interface.feedforward_tuning;
+
+import coppercore.controls.Tunable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class TuneS extends Command {
+    private Tunable subsystem;
+
+    private int slot;
+
+    double startPosition;
+
+    double appliedVolts;
+
+    public TuneS(Tunable subsystem, int slot) {
+        this.subsystem = subsystem;
+        this.slot = slot;
+
+        // this.withTimeout(5); TODO: Maybe add?
+    }
+
+    @Override
+    public void initialize() {
+        startPosition = subsystem.getPosition(slot);
+        appliedVolts = 0;
+    }
+
+    @Override
+    public void execute() {
+        subsystem.setVolts(appliedVolts, slot);
+        appliedVolts += 0.001;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        subsystem.setVolts(0.0, slot);
+        SmartDashboard.putNumber("Test-Mode/kS", appliedVolts);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return subsystem.getVelocity(slot) > 0.01;
+    }
+}
