4057fedbd4363ac7b033566f82b24631615de786
nathan <nathansmith@mcps.org>
2024-11-23 18:38:07 -0500

    Remove paths

--- wpilib_interface/src/main/java/frc/robot/BuildConstants.java
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "com.github.team401";
     public static final String MAVEN_NAME = "wpilib_interface";
     public static final String VERSION = "0.0";
-    public static final int GIT_REVISION = 32;
-    public static final String GIT_SHA = "158b251bd8f5c629336cdcb9613b31d1b610673d";
-    public static final String GIT_DATE = "2024-11-18 19:29:50 EST";
+    public static final int GIT_REVISION = 33;
+    public static final String GIT_SHA = "1b2d554bb5908f70bb23c283af21cbe4da88f822";
+    public static final String GIT_DATE = "2024-11-23 18:34:29 EST";
     public static final String GIT_BRANCH = "addJSONSaving";
-    public static final String BUILD_DATE = "2024-11-23 18:33:48 EST";
-    public static final long BUILD_UNIX_TIME = 1732404828551L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2024-11-23 18:37:13 EST";
+    public static final long BUILD_UNIX_TIME = 1732405033508L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
