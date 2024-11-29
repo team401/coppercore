3125eec9f44845b30ab58cb2c9d735531d37d245
aidnem <99768676+aidnem@users.noreply.github.com>
2024-11-29 13:45:41 -0500

    Add monitors (#31)
    * WIP - try to port monitors but the packages can't see each other yet so it's all broken
    
    * Fix MonitoredSubsystem but its still broken
    
    * Try to add AdvantageKit, now none of my imports work instead of only some of them
    
    * Turns out excluding wpilibj made us unable to import wpilibj, now just need to fix other import issues
    
    * Add builder pattern and fix tests for Monitor
    
    * Fix WPI imports and MAYBE fix advantagekit, must test out of codespace to confirm
    
    * GREAT SCOTT GLORY HALLELUJAH IT BUILDS
    
    * Remove to-do comment that had already been done
    
    * Simplify boolean logic in Monitor and improve code comments
    
    * Update normal comments to be doc comments for fields of Monitor
    
    * Remove gradleRIO and manually add wpilib dependencies instead
    
    * Add ability to enable and disable logging for MonitoredSubsystem and for each individual Monitor
    
    * Make MonitoredSubsystem an abstract class to force users to override monitoredPeriodic
    
    * Add tests for monitor fault callbacks
    
    * Fix wpilib_interface's build.gradle so that imports work again
    
    ---------
    
    Co-authored-by: aidnem <>

---
+++ wpilib_interface/src/main/java/coppercore/wpilib_interface/MonitoredSubsystem.java
@@ -0,0 +1,63 @@
+package coppercore.wpilib_interface;
+
+import coppercore.monitors.Monitor;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
+import java.util.List;
+import org.littletonrobotics.junction.Logger;
+
+public abstract class MonitoredSubsystem extends SubsystemBase {
+    private List<Monitor> registeredMonitors = new ArrayList<Monitor>();
+
+    private boolean loggingEnabled = true;
+
+    public void addMonitor(Monitor monitor) {
+        registeredMonitors.add(monitor);
+    }
+
+    @Override
+    public void periodic() {
+        monitoredPeriodic();
+        runMonitors();
+    }
+
+    /**
+     * OVERRIDE ME! This function is called every time the subsystem's periodic function is called.
+     * However, MonitoredSubsytem automatically checks monitors during every periodic run.
+     * Therefore, this method should be overridden as a replacement for the normal periodic function
+     * in the implementation of the subsystem.
+     *
+     * <p>This method is called periodically by the {@link CommandScheduler}. Useful for updating
+     * subsystem-specific state that you don't want to offload to a {@link Command}. Teams should
+     * try to be consistent within their own codebases about which responsibilities will be handled
+     * by Commands, and which will be handled here.
+     */
+    public abstract void monitoredPeriodic();
+
+    private void runMonitors() {
+        registeredMonitors.forEach(
+                monitor -> {
+                    monitor.periodic(Timer.getFPGATimestamp());
+
+                    if (loggingEnabled && monitor.getLoggingEnabled()) {
+                        Logger.recordOutput(
+                                "monitors/" + monitor.getName() + "/triggered",
+                                monitor.isTriggered());
+                        Logger.recordOutput(
+                                "monitors/" + monitor.getName() + "/faulted", monitor.isFaulted());
+                    }
+                });
+    }
+
+    /**
+     * Set whether or not the monitored subsystem should log its monitors. This is enabled by
+     * default, but can be disabled if there are RAM issues stemming from too many strings in
+     * logging.
+     *
+     * @param loggingEnabled
+     */
+    public void setLoggingEnabled(boolean loggingEnabled) {
+        this.loggingEnabled = loggingEnabled;
+    }
+}
