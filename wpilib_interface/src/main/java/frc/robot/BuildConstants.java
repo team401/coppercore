994c331160d947eb43b48725163bde420b991fb4
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2024-12-02 21:23:56 -0500

    Add Saving of JSON (#46)
    * Add Saving of JSON
    
    * Remove paths
    
    * Make json have pretty printing for test
    
    * Fix serialization of JSONExclude
    
    * Fixed Tests
    
    * Hopefully fix filepaths
    
    * Add Saving of JSON
    
    * Remove paths
    
    * Make json have pretty printing for test
    
    * Fix serialization of JSONExclude
    
    * Fixed Tests
    
    * Hopefully fix filepaths

---
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "com.github.team401";
+    public static final String MAVEN_NAME = "wpilib_interface";
+    public static final String VERSION = "0.0";
+    public static final int GIT_REVISION = 33;
+    public static final String GIT_SHA = "1b2d554bb5908f70bb23c283af21cbe4da88f822";
+    public static final String GIT_DATE = "2024-11-23 18:34:29 EST";
+    public static final String GIT_BRANCH = "addJSONSaving";
+    public static final String BUILD_DATE = "2024-11-23 18:37:13 EST";
+    public static final long BUILD_UNIX_TIME = 1732405033508L;
+    public static final int DIRTY = 0;
+
+    private BuildConstants() {}
+}
