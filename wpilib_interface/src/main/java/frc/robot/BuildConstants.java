58fc7d10f5dbeca0eb1c0465fcbb5d7acaa13750
aidnem <99768676+aidnem@users.noreply.github.com>
2024-12-06 09:45:01 -0500

    Update version string to 2025.0.0-beta

--- wpilib_interface/src/main/java/frc/robot/BuildConstants.java
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -2,16 +2,16 @@ package frc.robot;
 
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "com.github.team401";
+    public static final String MAVEN_GROUP = "io.github.team401.coppercore";
     public static final String MAVEN_NAME = "wpilib_interface";
-    public static final String VERSION = "0.0";
-    public static final int GIT_REVISION = 33;
-    public static final String GIT_SHA = "1b2d554bb5908f70bb23c283af21cbe4da88f822";
-    public static final String GIT_DATE = "2024-11-23 18:34:29 EST";
-    public static final String GIT_BRANCH = "addJSONSaving";
-    public static final String BUILD_DATE = "2024-11-23 18:37:13 EST";
-    public static final long BUILD_UNIX_TIME = 1732405033508L;
-    public static final int DIRTY = 0;
+    public static final String VERSION = "2025.0.0-beta";
+    public static final int GIT_REVISION = 43;
+    public static final String GIT_SHA = "60c0db4808cb5dcf1d9bb3c9a4ba41c86abf1d15";
+    public static final String GIT_DATE = "2024-12-05 12:25:16 EST";
+    public static final String GIT_BRANCH = "2025core";
+    public static final String BUILD_DATE = "2024-12-06 09:44:25 EST";
+    public static final long BUILD_UNIX_TIME = 1733496265888L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
