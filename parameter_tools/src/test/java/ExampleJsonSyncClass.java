ddc04e079cba49e2817a35376e6239576d1dba13
sleepyghost <145573597+sleepyghost-zzz@users.noreply.github.com>
2024-12-09 10:33:47 -0500

    Add javadoc for vision and parameter tools (#68)
    * Added Javadocs for all of vision.
    
    * Added Javadocs for all of parameter_tools

--- parameter_tools/src/test/java/ExampleJsonSyncClass.java
+++ parameter_tools/src/test/java/ExampleJsonSyncClass.java
@@ -2,24 +2,53 @@ package coppercore.parameter_tools.test;
 
 import coppercore.parameter_tools.JSONSync;
 
+/**
+ * Example class to demonstrate the usage of the {@link JSONSync} utility for saving and loading
+ * data in JSON format.
+ *
+ * <p>This class showcases how fields can be serialized and deserialized, including nested objects,
+ * using the {@link JSONSync} functionality.
+ *
+ * <p>Key features:
+ *
+ * <ul>
+ *   <li>Defines several fields of various types to demonstrate JSON serialization.
+ *   <li>Includes a nested class {@code BasicMotorDataHolder} to demonstrate handling nested data
+ *       structures.
+ *   <li>Utilizes {@link JSONSync} to manage serialization and deserialization with the associated
+ *       JSON file.
+ * </ul>
+ */
 public class ExampleJsonSyncClass {
 
+    /**
+     * Static {@link JSONSync} instance for managing the serialization and deserialization of this
+     * class.
+     */
     public static JSONSync<ExampleJsonSyncClass> synced =
-            new JSONSync<ExampleJsonSyncClass>(
+            new JSONSync<>(
                     new ExampleJsonSyncClass(),
                     "filePath",
                     new JSONSync.JSONSyncConfigBuilder().build());
 
     public final String testText = "";
+
     public final Double testDouble = 0.0;
+
     public Integer testInt = 0;
+
     public final BasicMotorDataHolder motorData = null;
 
+    /** Nested class to represent motor-related data. */
     public class BasicMotorDataHolder {
+
         public final Double maxVoltage = 0.0;
+
         public final Double minVoltage = 0.0;
+
         public Double currentVoltage = 0.0;
 
+        @Override
         public String toString() {
             return "minVoltage: "
                     + minVoltage
@@ -30,6 +59,7 @@ public class ExampleJsonSyncClass {
         }
     }
 
+    @Override
     public String toString() {
         return "testText: "
                 + testText
