35288adc091c5083adf172c310e10b5ef8ca2b8a
Jack Lingle <captainjack0818@gmail.com>
2024-12-11 08:25:07 -0500

    add build.gradle file for wpilib interface

--- wpilib_interface/src/main/java/frc/robot/BuildConstants.java
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "io.github.team401.coppercore";
     public static final String MAVEN_NAME = "wpilib_interface";
     public static final String VERSION = "2025.0.0-beta";
-    public static final int GIT_REVISION = 47;
-    public static final String GIT_SHA = "62c754b6cfc3b48dd5668227ea6baec2b09f5838";
-    public static final String GIT_DATE = "2024-12-09 19:45:39 EST";
+    public static final int GIT_REVISION = 48;
+    public static final String GIT_SHA = "d6d400d6c707336995b8a6c1207d05cb6242e342";
+    public static final String GIT_DATE = "2024-12-10 08:37:52 EST";
     public static final String GIT_BRANCH = "ak-vision";
-    public static final String BUILD_DATE = "2024-12-10 08:37:13 EST";
-    public static final long BUILD_UNIX_TIME = 1733837833077L;
+    public static final String BUILD_DATE = "2024-12-11 08:24:52 EST";
+    public static final long BUILD_UNIX_TIME = 1733923492432L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
