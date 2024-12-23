32b35f9263c6b44e36a674758abfa906d2a37502
minhnguyenbhs <minhnguyen@mcps.org>
2024-12-22 23:48:44 -0500

    ok i'm going to commit this because even though it's still a wip it's a milestone- thanks aiden!!

--- wpilib_interface/src/main/java/frc/robot/BuildConstants.java
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "io.github.team401.coppercore";
     public static final String MAVEN_NAME = "wpilib_interface";
     public static final String VERSION = "2025.0.0-beta";
-    public static final int GIT_REVISION = 44;
-    public static final String GIT_SHA = "ddc04e079cba49e2817a35376e6239576d1dba13";
-    public static final String GIT_DATE = "2024-12-09 10:33:47 EST";
+    public static final int GIT_REVISION = 45;
+    public static final String GIT_SHA = "531a9e363639e92e794307ba38162963d2a9cb38";
+    public static final String GIT_DATE = "2024-12-22 16:56:37 EST";
     public static final String GIT_BRANCH = "73-add-logged-tunable-number-to-coppercore";
-    public static final String BUILD_DATE = "2024-12-22 16:53:37 EST";
-    public static final long BUILD_UNIX_TIME = 1734904417196L;
+    public static final String BUILD_DATE = "2024-12-22 23:43:32 EST";
+    public static final long BUILD_UNIX_TIME = 1734929012028L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
