425299c1844d81416bc5ec5ad8c7b25bac133f49
minhnguyenbhs <minhnguyen@mcps.org>
2024-12-26 21:53:24 -0500

    it builds and has been published to maven local but hasn't been tested yet due to issues between advantagekit and wpilib versions

--- wpilib_interface/src/main/java/frc/robot/BuildConstants.java
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "io.github.team401.coppercore";
     public static final String MAVEN_NAME = "wpilib_interface";
     public static final String VERSION = "2025.0.0-beta";
-    public static final int GIT_REVISION = 45;
-    public static final String GIT_SHA = "531a9e363639e92e794307ba38162963d2a9cb38";
-    public static final String GIT_DATE = "2024-12-22 16:56:37 EST";
+    public static final int GIT_REVISION = 46;
+    public static final String GIT_SHA = "32b35f9263c6b44e36a674758abfa906d2a37502";
+    public static final String GIT_DATE = "2024-12-22 23:48:44 EST";
     public static final String GIT_BRANCH = "73-add-logged-tunable-number-to-coppercore";
-    public static final String BUILD_DATE = "2024-12-22 23:43:32 EST";
-    public static final long BUILD_UNIX_TIME = 1734929012028L;
+    public static final String BUILD_DATE = "2024-12-26 21:42:39 EST";
+    public static final long BUILD_UNIX_TIME = 1735267359194L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
