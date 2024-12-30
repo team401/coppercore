03309216c995611dacfc66eb0d438aa6a44740af
Jack Lingle <captainjack0818@gmail.com>
2024-12-30 13:03:35 -0500

    Set up Vision according to advantage kit (#69)

--- vision/src/main/java/coppercore/vision/CoreVisionConstants.java
+++ vision/src/main/java/coppercore/vision/CoreVisionConstants.java
@@ -1,10 +1,5 @@
 package coppercore.vision;
 
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-
 /**
  * Vision constants for coppercore's internal vision constants. Not to be confused with
  * VisionConstants, which is likely your robot project's local vision constants that are configured
@@ -14,10 +9,9 @@ public final class CoreVisionConstants {
     // TODO: Tune this value.
     // This value mainly exists so that this file stays named CoreVisionConstants.
     public static final double maxAcceptedDistanceMeters = 10.0;
-    public static final Matrix<N3, N1> singleTagStdDev = VecBuilder.fill(1, 1, 1);
-    public static final Matrix<N3, N1> multiTagStdDev = VecBuilder.fill(0.5, 0.5, 0.5);
-    public static final Matrix<N3, N1> rejectionStdDev =
-            VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+    public static final double linearStdDevFactor = 0.02;
+    public static final double angularStdDevFactor = 0.06;
     public static final double singleTagDistanceCutoff = 4.0;
-    public static final double distanceFactor = 30.0;
+    public static final double distanceFactor = 1.0;
+    public static final double maxZCutoff = 1.0;
 }
