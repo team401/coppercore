6cef33e13416477148558efe7a8406bd8a00b458
Samson560915 <rubbersboa@gmail.com>
2025-01-11 17:31:58 -0500

    Added javadocs for geometry

--- geometry/src/main/java/coppercore/Point.java
+++ geometry/src/main/java/coppercore/Point.java
@@ -1,18 +1,22 @@
 package coppercore.geometry;
 
+/** This class sets a point and gets its x and y values. */
 public class Point {
     private double x;
     private double y;
 
+    /** This sets a point */
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /** This gets the point's x value */
     public double getX() {
         return x;
     }
 
+    /** This gets the point's y value */
     public double getY() {
         return y;
     }
