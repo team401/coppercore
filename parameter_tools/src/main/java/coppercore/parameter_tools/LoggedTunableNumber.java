f83ecffb2b7f235e4fd6e75edc95d240dbbc6030
nathan <nathansmith@mcps.org>
2025-01-11 14:51:08 -0500

    Fix package I think

--- parameter_tools/src/main/java/coppercore/parameter_tools/LoggedTunableNumber.java
+++ parameter_tools/src/main/java/coppercore/parameter_tools/LoggedTunableNumber.java
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
-package coppercore.controls;
+package coppercore.parameter_tools;
 
 import java.util.Arrays;
 import java.util.HashMap;
