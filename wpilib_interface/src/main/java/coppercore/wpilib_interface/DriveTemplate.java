06122ddec3432fc74b8fe0ebc26c7df6d627a166
Jack Lingle <captainjack0818@gmail.com>
2025-01-13 10:42:04 -0500

    add back boolean to DriveTemplate

--- wpilib_interface/src/main/java/coppercore/wpilib_interface/DriveTemplate.java
+++ wpilib_interface/src/main/java/coppercore/wpilib_interface/DriveTemplate.java
@@ -4,5 +4,5 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public interface DriveTemplate extends Subsystem {
-    public void setGoalSpeeds(ChassisSpeeds goalSpeeds);
+    public void setGoalSpeeds(ChassisSpeeds goalSpeeds, boolean fieldCentric);
 }
