fa8afb07b3dc0a5fdc1b83d5774a383b02b69d8f
aidnem <99768676+aidnem@users.noreply.github.com>
2025-01-14 19:33:22 -0500

    Add Tunable, TuneG, TuneS, and TuneV

---
+++ wpilib_interface/src/main/java/coppercore/wpilib_interface/tuning/TuneG.java
@@ -0,0 +1,48 @@
+// TODO: WIP - Not tested
+
+package coppercore.wpilib_interface.tuning;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class TuneG extends Command {
+    private Tunable subsystem;
+
+    private int slot;
+
+    double startPosition;
+
+    double kG;
+    double kS;
+
+    public TuneG(Tunable subsystem, int slot) {
+        this.subsystem = subsystem;
+        this.kS = SmartDashboard.getNumber("Test-Mode/kS", 0);
+        this.slot = slot;
+
+        // this.withTimeout(5); TODO: Maybe add?
+    }
+
+    @Override
+    public void initialize() {
+        startPosition = subsystem.getPosition(slot);
+        kG = kS;
+    }
+
+    @Override
+    public void execute() {
+        subsystem.setVolts(kG, slot);
+        kG += 0.001;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        subsystem.setVolts(0.0, slot);
+        SmartDashboard.putNumber("Test-Mode/kG", kG - kS);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return subsystem.getPosition(slot) > Math.abs(startPosition - 0.1);
+    }
+}
