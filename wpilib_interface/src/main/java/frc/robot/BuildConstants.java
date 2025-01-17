1732771898a1f29eef13f4fae770bc05e962b54f
minhnguyenbhs <minhnguyen@mcps.org>
2025-01-17 18:27:12 -0500

    I got rid of one of the requierements

--- wpilib_interface/src/main/java/frc/robot/BuildConstants.java
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "io.github.team401.coppercore";
     public static final String MAVEN_NAME = "wpilib_interface";
     public static final String VERSION = "2025.0.0-beta";
-    public static final int GIT_REVISION = 46;
-    public static final String GIT_SHA = "c3ac0dc515f33e84e178ad1f6c9be34e081546df";
-    public static final String GIT_DATE = "2025-01-16 19:43:46 EST";
+    public static final int GIT_REVISION = 47;
+    public static final String GIT_SHA = "658a4184c7198b38167afa9b5607b811c05f8b19";
+    public static final String GIT_DATE = "2025-01-16 19:55:34 EST";
     public static final String GIT_BRANCH = "add-math-gear-functions";
-    public static final String BUILD_DATE = "2025-01-16 19:47:19 EST";
-    public static final long BUILD_UNIX_TIME = 1737074839051L;
+    public static final String BUILD_DATE = "2025-01-17 18:24:55 EST";
+    public static final long BUILD_UNIX_TIME = 1737156295159L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
