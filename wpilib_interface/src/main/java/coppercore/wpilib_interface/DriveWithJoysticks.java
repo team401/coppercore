7354ce9f8982f9a3ecfd4dcfa974bc294a64d74c
Jack Lingle <captainjack0818@gmail.com>
2025-01-18 16:08:10 -0500

    fic clamp inputs

--- wpilib_interface/src/main/java/coppercore/wpilib_interface/DriveWithJoysticks.java
+++ wpilib_interface/src/main/java/coppercore/wpilib_interface/DriveWithJoysticks.java
@@ -38,9 +38,9 @@ public class DriveWithJoysticks extends Command {
     @Override
     public void execute() {
         // clamp inputs between 0 and 1 to prevent crazy speeds
-        double leftJoystickX = MathUtil.clamp(leftJoystick.getX(), 0, 1);
-        double leftJoystickY = MathUtil.clamp(leftJoystick.getY(), 0, 1);
-        double rightJoystickX = MathUtil.clamp(rightJoystick.getX(), 0, 1);
+        double leftJoystickX = MathUtil.clamp(leftJoystick.getX(), -1, 1);
+        double leftJoystickY = MathUtil.clamp(leftJoystick.getY(), -1, 1);
+        double rightJoystickX = MathUtil.clamp(rightJoystick.getX(), -1, 1);
 
         Translation2d linearSpeeds = getLinearVelocity(-leftJoystickX, -leftJoystickY);
 
