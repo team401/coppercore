1b86a08dba4edb1d8e68ba251188b8b9190cc5a4
minhnguyenbhs <minhnguyen@mcps.org>
2025-01-17 19:24:36 -0500

    Broken Code import broken for unit

--- math/src/main/java/coppercore/math/GearConversionFunctions.java
+++ math/src/main/java/coppercore/math/GearConversionFunctions.java
@@ -1,45 +1,47 @@
 package coppercore.math;
 
+import static edu.wpi.first.units.Units.*;
+
 public class GearConversionFunctions {
     public static double pitchDiameterFrom3mmPulley(int teeth) {
         teeth = Math.abs(teeth);
-        double pitchDiameter = 3 * teeth / ((double) Math.PI);
+        Distance pitchDiameter = Millimeters.of(3 * teeth / ((double) Math.PI));
         return pitchDiameter;
     }
 
     public static double pitchDiameterFrom5mmPulley(int teeth) {
         teeth = Math.abs(teeth);
-        double pitchDiameter = 5 * teeth / ((double) Math.PI);
+        Distance pitchDiameter = Millimeters.of(5 * teeth / ((double) Math.PI));
         return pitchDiameter;
     }
 
     public static double pitchDiameterFromRT25Pulley(int teeth) {
         teeth = Math.abs(teeth);
-        double pitchDiameter = 0.25 * teeth / ((double) Math.PI);
+        Distance pitchDiameter = Inches.of(0.25 * teeth / ((double) Math.PI));
         return pitchDiameter;
     }
 
     public static double pitchDiameterFrom25ChainSprocket(int teeth) {
         teeth = Math.abs(teeth);
-        double pitchDiameter = 0.25 / (double) Math.sin(Math.PI / teeth);
+        Distance pitchDiameter = Inches.of(0.25 / (double) Math.sin(Math.PI / teeth));
         return pitchDiameter;
     }
 
     public static double pitchDiameterFrom35ChainSprocket(int teeth) {
         teeth = Math.abs(teeth);
-        double pitchDiameter = 0.375 / (double) Math.sin(Math.PI / teeth);
+        Distance pitchDiameter = Inches.of(0.375 / (double) Math.sin(Math.PI / teeth));
         return pitchDiameter;
     }
 
     public static double pitchDiameterFrom20DPGear(int teeth) {
         teeth = Math.abs(teeth);
-        double pitchDiameter = (double) teeth / 20;
+        Distance pitchDiameter = Inches.of((double) teeth / 20);
         return pitchDiameter;
     }
 
     public static double pitchDiameterFrom32DPGear(int teeth) {
         teeth = Math.abs(teeth);
-        double pitchDiameter = (double) teeth / 32;
+        Distance pitchDiameter = Inches.of((double) teeth / 32);
         return pitchDiameter;
     }
 }
--- wpilib_interface/src/main/java/frc/robot/BuildConstants.java
+++ wpilib_interface/src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "io.github.team401.coppercore";
     public static final String MAVEN_NAME = "wpilib_interface";
     public static final String VERSION = "2025.0.0-beta";
-    public static final int GIT_REVISION = 47;
-    public static final String GIT_SHA = "658a4184c7198b38167afa9b5607b811c05f8b19";
-    public static final String GIT_DATE = "2025-01-16 19:55:34 EST";
+    public static final int GIT_REVISION = 48;
+    public static final String GIT_SHA = "1732771898a1f29eef13f4fae770bc05e962b54f";
+    public static final String GIT_DATE = "2025-01-17 18:27:12 EST";
     public static final String GIT_BRANCH = "add-math-gear-functions";
-    public static final String BUILD_DATE = "2025-01-17 18:24:55 EST";
-    public static final long BUILD_UNIX_TIME = 1737156295159L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-01-17 19:05:23 EST";
+    public static final long BUILD_UNIX_TIME = 1737158723551L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
