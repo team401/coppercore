ca5bbaa42591faf8d3c41963b62da0c81e4f3d95
nathan <nathansmith@mcps.org>
2025-01-20 13:29:56 -0500

    WIP Units

---
+++ parameter_tools/src/main/java/coppercore/parameter_tools/TypeAdapters/JSONDistance.java
@@ -0,0 +1,30 @@
+package coppercore.parameter_tools.TypeAdapters;
+
+import coppercore.parameter_tools.JSONObject;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.DistanceUnit;
+import edu.wpi.first.units.Units;
+
+
+public class JSONDistance extends JSONObject<Distance> {
+    double value;
+    String unit;
+
+    public JSONDistance(Distance distance) {
+        super(distance);
+        value = distance.magnitude();
+        unit = distance.unit().name();
+    }
+
+    @Override
+    public Distance toJava() {
+        switch (unit){
+            case "Meter":
+                return Units.Meters.of(value);
+            case "Millimeter":
+                return Units.Millimeters.of(value);
+            default:
+                throw new RuntimeException(unit+" does not exist");
+        }
+    }
+}
