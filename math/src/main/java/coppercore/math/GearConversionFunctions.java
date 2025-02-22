f38f94b6446459ae37fc6f79b34caae335d5c02a
minhnguyenbhs <minhnguyen@mcps.org>
2025-02-22 13:35:38 -0500

    I think this works.

--- math/src/main/java/coppercore/math/GearConversionFunctions.java
+++ math/src/main/java/coppercore/math/GearConversionFunctions.java
@@ -1,6 +1,9 @@
 package coppercore.math;
 
-import edu.first.wpilib.units.Distance;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Millimeters;
+
+import edu.wpi.first.units.measure.Distance;
 
 public class GearConversionFunctions {
     public static Distance pitchDiameterFrom3mmPulley(int teeth) {
@@ -9,37 +12,37 @@ public class GearConversionFunctions {
         return pitchDiameter;
     }
 
-    public static double pitchDiameterFrom5mmPulley(int teeth) {
+    public static Distance pitchDiameterFrom5mmPulley(int teeth) {
         teeth = Math.abs(teeth);
         Distance pitchDiameter = Millimeters.of(5 * teeth / ((double) Math.PI));
         return pitchDiameter;
     }
 
-    public static double pitchDiameterFromRT25Pulley(int teeth) {
+    public static Distance pitchDiameterFromRT25Pulley(int teeth) {
         teeth = Math.abs(teeth);
         Distance pitchDiameter = Inches.of(0.25 * teeth / ((double) Math.PI));
         return pitchDiameter;
     }
 
-    public static double pitchDiameterFrom25ChainSprocket(int teeth) {
+    public static Distance pitchDiameterFrom25ChainSprocket(int teeth) {
         teeth = Math.abs(teeth);
         Distance pitchDiameter = Inches.of(0.25 / (double) Math.sin(Math.PI / teeth));
         return pitchDiameter;
     }
 
-    public static double pitchDiameterFrom35ChainSprocket(int teeth) {
+    public static Distance pitchDiameterFrom35ChainSprocket(int teeth) {
         teeth = Math.abs(teeth);
         Distance pitchDiameter = Inches.of(0.375 / (double) Math.sin(Math.PI / teeth));
         return pitchDiameter;
     }
 
-    public static double pitchDiameterFrom20DPGear(int teeth) {
+    public static Distance pitchDiameterFrom20DPGear(int teeth) {
         teeth = Math.abs(teeth);
         Distance pitchDiameter = Inches.of((double) teeth / 20);
         return pitchDiameter;
     }
 
-    public static double pitchDiameterFrom32DPGear(int teeth) {
+    public static Distance pitchDiameterFrom32DPGear(int teeth) {
         teeth = Math.abs(teeth);
         Distance pitchDiameter = Inches.of((double) teeth / 32);
         return pitchDiameter;
--- math/src/test/java/coppercore/math/GearConversionTest.java
+++ math/src/test/java/coppercore/math/GearConversionTest.java
@@ -1,5 +1,8 @@
 package coppercore.math;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Millimeters;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,48 +12,72 @@ public class GearConversionTest {
     @Test
     public void pitchDiameterFrom3mmPulleyTest() {
         Assertions.assertEquals(
-                12 / (Math.PI), GearConversionFunctions.pitchDiameterFrom3mmPulley(4), delta);
+                12 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom3mmPulley(4).in(Millimeters),
+                delta);
         Assertions.assertEquals(
-                150 / (Math.PI), GearConversionFunctions.pitchDiameterFrom3mmPulley(50), delta);
+                150 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom3mmPulley(50).in(Millimeters),
+                delta);
         Assertions.assertEquals(
-                0 / (Math.PI), GearConversionFunctions.pitchDiameterFrom3mmPulley(0), delta);
+                0 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom3mmPulley(0).in(Millimeters),
+                delta);
         Assertions.assertEquals(
-                21 / (Math.PI), GearConversionFunctions.pitchDiameterFrom3mmPulley(-7), delta);
+                21 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom3mmPulley(-7).in(Millimeters),
+                delta);
     }
 
     @Test
     public void pitchDiameterFrom5mmPulleyTest() {
         Assertions.assertEquals(
-                20 / (Math.PI), GearConversionFunctions.pitchDiameterFrom5mmPulley(4), delta);
+                20 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom5mmPulley(4).in(Millimeters),
+                delta);
         Assertions.assertEquals(
-                250 / (Math.PI), GearConversionFunctions.pitchDiameterFrom5mmPulley(50), delta);
+                250 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom5mmPulley(50).in(Millimeters),
+                delta);
         Assertions.assertEquals(
-                0 / (Math.PI), GearConversionFunctions.pitchDiameterFrom5mmPulley(0), delta);
+                0 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom5mmPulley(0).in(Millimeters),
+                delta);
         Assertions.assertEquals(
-                35 / (Math.PI), GearConversionFunctions.pitchDiameterFrom5mmPulley(-7), delta);
+                35 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFrom5mmPulley(-7).in(Millimeters),
+                delta);
     }
 
     @Test
     public void pitchDiameterFromRT25PulleyTest() {
         Assertions.assertEquals(
-                1 / (Math.PI), GearConversionFunctions.pitchDiameterFromRT25Pulley(4), delta);
+                1 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFromRT25Pulley(4).in(Inches),
+                delta);
         Assertions.assertEquals(
-                13 / (Math.PI), GearConversionFunctions.pitchDiameterFromRT25Pulley(52), delta);
+                13 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFromRT25Pulley(52).in(Inches),
+                delta);
         Assertions.assertEquals(
-                0 / (Math.PI), GearConversionFunctions.pitchDiameterFromRT25Pulley(0), delta);
+                0 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFromRT25Pulley(0).in(Inches),
+                delta);
         Assertions.assertEquals(
-                2 / (Math.PI), GearConversionFunctions.pitchDiameterFromRT25Pulley(-8), delta);
+                2 / (Math.PI),
+                GearConversionFunctions.pitchDiameterFromRT25Pulley(-8).in(Inches),
+                delta);
     }
 
     @Test
     public void pitchDiameterFrom25ChainSprocketTest() {
         Assertions.assertEquals(
                 0.25 / (1 / Math.sqrt(2)),
-                GearConversionFunctions.pitchDiameterFrom25ChainSprocket(4),
+                GearConversionFunctions.pitchDiameterFrom25ChainSprocket(4).in(Inches),
                 delta);
         Assertions.assertEquals(
                 0.25 / (Math.sqrt(3) / 2),
-                GearConversionFunctions.pitchDiameterFrom25ChainSprocket(3),
+                GearConversionFunctions.pitchDiameterFrom25ChainSprocket(3).in(Inches),
                 delta);
     }
 
@@ -58,27 +85,35 @@ public class GearConversionTest {
     public void pitchDiameterFrom35ChainSprocketTest() {
         Assertions.assertEquals(
                 0.375 / (1 / Math.sqrt(2)),
-                GearConversionFunctions.pitchDiameterFrom35ChainSprocket(4),
+                GearConversionFunctions.pitchDiameterFrom35ChainSprocket(4).in(Inches),
                 delta);
         Assertions.assertEquals(
                 0.375 / (Math.sqrt(3) / 2),
-                GearConversionFunctions.pitchDiameterFrom35ChainSprocket(3),
+                GearConversionFunctions.pitchDiameterFrom35ChainSprocket(3).in(Inches),
                 delta);
     }
 
     @Test
     public void pitchDiameterFrom20DPGearTest() {
-        Assertions.assertEquals(0.2, GearConversionFunctions.pitchDiameterFrom20DPGear(4), delta);
-        Assertions.assertEquals(1, GearConversionFunctions.pitchDiameterFrom20DPGear(20), delta);
-        Assertions.assertEquals(0, GearConversionFunctions.pitchDiameterFrom20DPGear(0), delta);
-        Assertions.assertEquals(2.0, GearConversionFunctions.pitchDiameterFrom20DPGear(-40), delta);
+        Assertions.assertEquals(
+                0.2, GearConversionFunctions.pitchDiameterFrom20DPGear(4).in(Inches), delta);
+        Assertions.assertEquals(
+                1, GearConversionFunctions.pitchDiameterFrom20DPGear(20).in(Inches), delta);
+        Assertions.assertEquals(
+                0, GearConversionFunctions.pitchDiameterFrom20DPGear(0).in(Inches), delta);
+        Assertions.assertEquals(
+                2.0, GearConversionFunctions.pitchDiameterFrom20DPGear(-40).in(Inches), delta);
     }
 
     @Test
     public void pitchDiameterFrom32DPGearTest() {
-        Assertions.assertEquals(0.125, GearConversionFunctions.pitchDiameterFrom32DPGear(4), delta);
-        Assertions.assertEquals(1.0, GearConversionFunctions.pitchDiameterFrom32DPGear(32), delta);
-        Assertions.assertEquals(0, GearConversionFunctions.pitchDiameterFrom32DPGear(0), delta);
-        Assertions.assertEquals(2.0, GearConversionFunctions.pitchDiameterFrom32DPGear(-64), delta);
+        Assertions.assertEquals(
+                0.125, GearConversionFunctions.pitchDiameterFrom32DPGear(4).in(Inches), delta);
+        Assertions.assertEquals(
+                1.0, GearConversionFunctions.pitchDiameterFrom32DPGear(32).in(Inches), delta);
+        Assertions.assertEquals(
+                0, GearConversionFunctions.pitchDiameterFrom32DPGear(0).in(Inches), delta);
+        Assertions.assertEquals(
+                2.0, GearConversionFunctions.pitchDiameterFrom32DPGear(-64).in(Inches), delta);
     }
 }
