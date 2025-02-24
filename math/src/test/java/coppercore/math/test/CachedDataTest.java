4c4e91be9d263b7770f41bd41665b3e53e9b6c80
minhnguyenbhs <minhnguyen@mcps.org>
2025-02-24 18:52:09 -0500

    Units are test

--- math/src/test/java/coppercore/math/test/CachedDataTest.java
+++ math/src/test/java/coppercore/math/test/CachedDataTest.java
@@ -1,5 +1,7 @@
 package coppercore.math.test;
 
+import static edu.wpi.first.units.Units.Millimeters;
+
 import coppercore.math.CachedData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -26,17 +28,23 @@ public class CachedDataTest {
     @Test
     public void readTest() {
         CachedData data = new CachedData(10);
+        // 1
         data.write("real");
         Assertions.assertEquals(data.read(), "real");
-        // data.write(null);
-        // Assertions.assertEquals(data.read(), null);
+        // 2
         data.write("fake");
         Assertions.assertEquals(data.read(), "fake");
+        // 3
         data.write(100);
         Assertions.assertEquals(data.read(), 100);
+        // 4
         data.write(true);
         Assertions.assertEquals(data.read(), true);
+        // 5
         data.write(false);
         Assertions.assertEquals(data.read(), false);
+        // 6
+        data.write(Millimeters.of(5));
+        Assertions.assertEquals(data.read(), Millimeters.of(5));
     }
 }
