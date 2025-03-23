329e8faec29dc74863549216e59f764ef51805da
Samson560915 <rubbersboa@gmail.com>
2025-03-23 13:59:27 -0400

    Added Javadocs

--- math/src/main/java/coppercore/math/CachedData.java
+++ math/src/main/java/coppercore/math/CachedData.java
@@ -1,5 +1,8 @@
 package coppercore.math;
 
+/**
+This class is used to keep track of the cached data
+*/
 public class CachedData<Type> {
 
     private Type value = null;
@@ -8,28 +11,38 @@ public class CachedData<Type> {
     private double lastUpdateTime = 0.0;
     private double staleTime = -1.0;
     private boolean isTimeBased = true;
-
+    
+    /**
+    This class is a constructor for the Time-based expiration
+    @param staleTime This is the amount of time it takes a cache to be stale
+    */
     // Constructor for Time-based expiration
     public CachedData(double staleTime) {
         this.isTimeBased = true;
         this.staleTime = staleTime * 1000; // Convert seconds to milliseconds
         this.lastUpdateTime = System.currentTimeMillis();
     }
-
+    
+    /**
+    This class is a constructor for the readCount-based expiration
+    @param maxReads This is the maximum amount of reads we can take before the cache is stale
+    */
     // Constructor for readCount-based expiration
     public CachedData(int maxReads) {
         this.isTimeBased = false;
         this.maxReads = maxReads;
         this.readCount = 0;
     }
-
+    
+    /**This class updates the value based on the change*/
     // Update the value based on the change (for both time and read-based mechanisms)
     private void update() {
         if (isStale()) {
             value = null;
         }
     }
-
+    
+    /**This is used to write a new signal value(A new cache)*/
     // Write a new signal value
     public void write(Type signal) {
         if (signal != null) {
@@ -37,7 +50,11 @@ public class CachedData<Type> {
             reset(); // Resetting the expiration count or time
         }
     }
-
+    
+    /**
+    This reads the cached value and returns it
+    @return this returns the cached value
+    */
     // Read the cached value, returning null if the cache is stale
     public Type read() {
         update();
@@ -45,6 +62,10 @@ public class CachedData<Type> {
         return value; // Return the cached value
     }
 
+    /**
+    This class checks if the cache is stale
+    @return A boolean for whether the cache is stale or not
+    */
     // Check if the cache has expired (is stale)
     public boolean isStale() {
         if (isTimeBased) {
@@ -54,7 +75,10 @@ public class CachedData<Type> {
             return readCount >= maxReads; // Check if the read count has exceeded
         }
     }
-
+    
+    /**
+    This resets the expiration tracking value
+    */
     // Reset the expiration tracking
     private void reset() {
         if (isTimeBased) {
