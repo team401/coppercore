d47b5506ab284d0814062bd15d613081afedbfb5
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2025-04-02 10:32:40 -0400

    fix stupid mistake

--- parameter_tools/src/main/java/coppercore/parameter_tools/json/JSONNamingStrategy.java
+++ parameter_tools/src/main/java/coppercore/parameter_tools/json/JSONNamingStrategy.java
@@ -4,6 +4,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.FieldNamingStrategy;
 import edu.wpi.first.units.Measure;
 import java.lang.reflect.Field;
+import java.lang.Throwable;
 
 public class JSONNamingStrategy implements FieldNamingStrategy {
 
@@ -44,7 +45,7 @@ public class JSONNamingStrategy implements FieldNamingStrategy {
                                     + " SuperClass: "
                                     + field.getDeclaringClass().getSuperclass().getName());
                     );
-                    (new Throwable).printStackTrace(System.out);
+                    (new Throwable()).printStackTrace(System.out);
                 }
                 if (config.primitiveCheckCrash()) {
                     throw new RuntimeException(
