4386c9afe69371374558a1707f7ee4da2c571f31
avidraccoon <nathansmith@mcps.org>
2025-06-29 16:48:28 -0400

    Remove unwanted text at top of file

--- parameter_tools/src/test/java/coppercore/parameter_tools/test/PathProviderTests.java
+++ parameter_tools/src/test/java/coppercore/parameter_tools/test/PathProviderTests.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5Suite.java to edit this template
- */
-
 package coppercore.parameter_tools.test;
 
 import java.io.File;
