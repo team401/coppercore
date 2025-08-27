e19c2198aa3ee64a72d3190371548b0705e66825
Godmar Back <gback@cs.vt.edu>
2025-08-27 18:57:04 -0400

    added gemini-generated unit tests for primitive crash feature

--- parameter_tools/src/main/java/coppercore/parameter_tools/json/strategies/JSONPrimitiveCheckStrategy.java
+++ parameter_tools/src/main/java/coppercore/parameter_tools/json/strategies/JSONPrimitiveCheckStrategy.java
@@ -1,14 +1,12 @@
 package coppercore.parameter_tools.json.strategies;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.FieldNamingStrategy;
-
 import coppercore.parameter_tools.json.JSONSyncConfig;
 import coppercore.parameter_tools.json.JSONSyncConfigBuilder;
 import coppercore.parameter_tools.json.annotations.JSONName;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class JSONPrimitiveCheckStrategy {
 
