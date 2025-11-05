d240d245f4c3351a6cf708fa3479365082ca3339
avidraccoon <nathansmith@mcps.org>
2025-11-05 13:11:11 -0500

    Enhance state machine classes with detailed Javadoc comments and improve transition handling methods

--- controls/src/main/java/coppercore/controls/state_machine/FunctionalState.java
+++ controls/src/main/java/coppercore/controls/state_machine/FunctionalState.java
@@ -1,14 +1,24 @@
 package coppercore.controls.state_machine;
 
+/**
+ * A state that executes a provided periodic function.
+ */
 public class FunctionalState<StateKey extends Enum<StateKey>> extends State<StateKey> {
 
     private final Runnable periodicFunction;
 
+    /**
+     * Constructs a FunctionalState with the given periodic function.
+     * @param periodicFunction The function to be executed periodically while in this state
+     */
     public FunctionalState(Runnable periodicFunction) {
         super();
         this.periodicFunction = periodicFunction;
     }
 
+    /**
+     * Executes the periodic function.
+     */
     @Override
     protected void periodic() {
         periodicFunction.run();
--- controls/src/main/java/coppercore/controls/state_machine/StateMachine.java
+++ controls/src/main/java/coppercore/controls/state_machine/StateMachine.java
@@ -3,27 +3,51 @@ package coppercore.controls.state_machine;
 import java.util.HashMap;
 import java.util.Map;
 
+// Note: Some parts of the javadoc were written using Copilot
+
+/**
+ * A simple state machine implementation.
+ */
 public class StateMachine<StateKey extends Enum<StateKey>> {
 
     private State<StateKey> state;
     private StateKey stateKey;
     private final Map<StateKey, State<StateKey>> states;
 
+    /**
+     * Constructs a new StateMachine.
+     */
     public StateMachine() {
         this.states = new HashMap<>();
     }
 
+    /**
+     * Adds a new functional state to the state machine.
+     * @param state StateKey of the new state
+     * @param periodic The periodic function to be called while in this state
+     * @return The newly created state
+     */
     public State<StateKey> addState(StateKey state, Runnable periodic) {
         State<StateKey> newState = new FunctionalState<>(periodic);
         states.put(state, newState);
         return newState;
     }
 
+    /**
+     * Registers a new state to the state machine.
+     * @param stateKey StateKey of the new state
+     * @param state The state to be registered
+     * @return The registered state
+     */
     public State<StateKey> registerState(StateKey stateKey, State<StateKey> state) {
         states.put(stateKey, state);
         return state;
     }
 
+    /**
+     * Sets the current state of the state machine.
+     * @param newState The StateKey of the new state
+     */
     public void setState(StateKey newState) {
         if (newState == null) {
             return;
@@ -38,21 +62,35 @@ public class StateMachine<StateKey extends Enum<StateKey>> {
         }
     }
 
+    /**
+     * Gets the current state key of the state machine.
+     * @return The current StateKey
+     */
     public StateKey getCurrentStateKey() {
         return stateKey;
     }
 
-    public State getCurrentState() {
+    /**
+     * Gets the current state of the state machine.
+     * @return The current State
+     */
+    public State<StateKey> getCurrentState() {
         return state;
     }
 
+    /**
+     * Updates the state machine, transitioning to the next state if conditions are met.
+     */
     public void updateStates() {
         if (state == null) {
             return;
         }
-        setState(state.checkTransitions());
+        setState(state.getNextState());
     }
 
+    /**
+     * Calls the periodic function of the current state.
+     */
     public void periodic() {
         if (state != null) {
             state._periodic();
