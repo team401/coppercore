a4231ac110fe1f3198857f16f4537ec215d9c016
Godmar Back <godmar@gmail.com>
2025-12-17 15:03:13 -0500

    (168) added discrete event simulation

---
+++ controls/src/test/java/coppercore/controls/test/StateMachineTestsDES.java
@@ -0,0 +1,318 @@
+package coppercore.controls.test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import coppercore.controls.state_machine.State;
+import coppercore.controls.state_machine.StateMachine;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Version of state machine test with discrete event simulation.
+ */
+public class StateMachineTestsDES {
+
+    // This method exist to keep the Transition Test State Machine and the State Machine Graph State
+    // Machine the same
+    StateMachine<Robot> createTestStateMachine(Robot stateMachineWorld) {
+        StateMachine<Robot> stateMachine = new StateMachine<>(stateMachineWorld);
+
+        // Registering States
+        List.of(idleState, intakingState, warmingUpState, shootingState)
+                .forEach(stateMachine::registerState);
+
+        // Defining Transitions
+
+        // Transitions that can happen from Idle state
+        idleState
+                .when((Robot robot) -> robot.shouldIntake && !robot.hasNote, "should intake")
+                .transitionTo(intakingState);
+        idleState
+                .when((Robot robot) -> robot.shouldShoot && robot.hasNote, "should shoot")
+                .transitionTo(warmingUpState);
+
+        // Transitions that can happen from Intaking state
+        intakingState.whenFinished().transitionTo(idleState);
+        intakingState
+                .when((Robot robot) -> !robot.shouldIntake, "should not intake")
+                .transitionTo(idleState);
+
+        // Transitions that can happen from WarmingUp state
+        warmingUpState.whenFinished().transitionTo(shootingState);
+        warmingUpState
+                .when((Robot robot) -> !robot.shouldShoot, "should not shoot")
+                .transitionTo(idleState);
+        warmingUpState.whenRequested(idleState).transitionTo(idleState);
+
+        // Transitions that can happen from Shooting state
+        shootingState.whenFinished().transitionTo(idleState);
+        shootingState
+                .when((Robot robot) -> !robot.shouldShoot, "should not shoot")
+                .transitionTo(idleState);
+        shootingState.whenRequested(idleState).transitionTo(idleState);
+
+        // Setting initial state
+        stateMachine.setState(idleState);
+
+        return stateMachine;
+    }
+
+    @Test
+    public void StateMachineTransitionsTest() {
+
+        // ### Setting up the state machine
+        // Creating the State Machine
+        Robot stateMachineWorld = new Robot();
+
+        StateMachine<Robot> stateMachine = createTestStateMachine(stateMachineWorld);
+
+        DES sim = new DES();
+
+        // Run robot loop every 20ms
+        DES.Runnable robotLoop =
+                new DES.Runnable() {
+                    @Override
+                    public void run(int simulationTime) {
+                        stateMachine.periodic();
+                        stateMachine.updateStates();
+                        sim.schedule(simulationTime + 20, this);
+                    }
+                };
+        sim.schedule(0, robotLoop);
+
+        // Testing
+        assertSame(idleState, stateMachine.getCurrentState());
+        final Function<State, DES.Runnable> assertIn =
+                (state) -> _time -> assertSame(state, stateMachine.getCurrentState());
+
+        // ### Ensure that when there is no input, the state remains the same
+        sim.schedule(1, assertIn.apply(idleState));
+        // ### Test transition to Intaking
+        sim.schedule(
+                5,
+                (_time) -> {
+                    stateMachineWorld.shouldIntake = true;
+                });
+
+        sim.schedule(25, assertIn.apply(intakingState));
+
+        // ### Test transition back to Idle from Intaking when intaking is canceled
+        sim.schedule(
+                30,
+                (_time) -> {
+                    stateMachineWorld.shouldIntake = false;
+                });
+
+        sim.schedule(50, assertIn.apply(idleState));
+
+        // ### Return to Intaking and simulate note acquisition to test finishing transition
+        sim.schedule(
+                55,
+                (_time) -> {
+                    stateMachineWorld.shouldIntake = true;
+                });
+
+        // Ensure we are in Intaking state
+        sim.schedule(65, assertIn.apply(intakingState));
+
+        // Ensure it does not transition back to Idle without note acquisition
+        sim.schedule(85, assertIn.apply(intakingState));
+
+        // Simulate note acquisition
+        sim.schedule(
+                90,
+                (_time) -> {
+                    stateMachineWorld.hasNote = true;
+                });
+
+        sim.schedule(110, assertIn.apply(idleState));
+
+        // Reset shouldIntake for next test
+        sim.schedule(
+                115,
+                (_time) -> {
+                    stateMachineWorld.shouldIntake = false;
+                });
+
+        // ### Test transition to WarmingUp
+        sim.schedule(
+                125,
+                (_time) -> {
+                    stateMachineWorld.shouldShoot = true;
+                });
+
+        sim.schedule(145, assertIn.apply(warmingUpState));
+
+        // ### Test transition to Idle from WarmingUp when shooting is canceled
+        sim.schedule(
+                150,
+                (_time) -> {
+                    stateMachineWorld.shouldShoot = false;
+                });
+
+        // XXX FIXME. There's a problem here; going into the idle
+        // state takes two iterations of the loop; it fails with 161-179.
+        sim.schedule(181, assertIn.apply(idleState));
+
+        // ### Return to WarmingUp and simulate not having a note in warmup to test requested
+        // transition
+        sim.schedule(
+                185,
+                (_time) -> {
+                    stateMachineWorld.shouldShoot = true;
+                });
+        sim.schedule(201, assertIn.apply(warmingUpState));
+
+        // Simulate not having a note
+        sim.schedule(
+                205,
+                (_time) -> {
+                    stateMachineWorld.hasNote = false;
+                });
+
+        sim.schedule(221, assertIn.apply(idleState));
+
+        // Reset for next test
+        sim.schedule(
+                225,
+                (_time) -> {
+                    stateMachineWorld.shouldShoot = true;
+                    stateMachineWorld.hasNote = true;
+                });
+
+        /* CONTINUE HERE
+
+        // ### Test transition to Shooting
+        stateMachine.updateStates();
+        assertSame(warmingUpState, stateMachine.getCurrentState());
+        stateMachine.periodic();
+
+        stateMachine.updateStates();
+        assertSame(shootingState, stateMachine.getCurrentState());
+
+        // ### Test transition back to Idle from Shooting when shooting is canceled
+        stateMachineWorld.shouldShoot = false;
+        stateMachine.updateStates();
+        assertSame(idleState, stateMachine.getCurrentState());
+
+        // ### Return to Shooting and simulate not having a note in shooting to test finishing
+        // transition
+        stateMachineWorld.shouldShoot = true;
+        stateMachine.updateStates();
+
+        // Ensure we are in WarmingUp state
+        assertSame(warmingUpState, stateMachine.getCurrentState());
+
+        // Finish WarmingUp to get to Shooting
+        stateMachine.periodic();
+
+        // Move to Shooting state
+        stateMachine.updateStates();
+        assertSame(shootingState, stateMachine.getCurrentState());
+
+        // Simulate not having a note
+        stateMachineWorld.hasNote = false;
+        stateMachine.periodic();
+        stateMachine.updateStates();
+        assertSame(idleState, stateMachine.getCurrentState());
+        */
+
+        // Finally, run the simulation
+        sim.simulate(10000);
+    }
+
+    @Test
+    public void StateMachineGraphTest() {
+        Robot stateMachineWorld = new Robot();
+        StateMachine<Robot> stateMachine = createTestStateMachine(stateMachineWorld);
+
+        String outputFilePath =
+                new File("").getAbsolutePath()
+                        + File.separator
+                        + "build"
+                        + File.separator
+                        + "resources"
+                        + File.separator
+                        + "test"
+                        + File.separator
+                        + "StateMachineGraphTestOutput.dot";
+
+        try {
+            // Ensure the output directory exists
+            File outputFile = new File(outputFilePath);
+            outputFile.getParentFile().mkdirs();
+
+            // Ensure the file is created if it does not exist
+            if (!outputFile.exists()) {
+                if (!outputFile.createNewFile()) {
+                    fail("Failed to create the output file: " + outputFilePath);
+                }
+            }
+
+            try (PrintWriter pw = new PrintWriter(outputFilePath)) {
+                // Write the graphviz file
+                stateMachine.writeGraphvizFile(pw);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    public static class Robot {
+        public boolean shouldShoot = false;
+        public boolean shouldIntake = false;
+        public boolean hasNote = false;
+        public double motorSpeed = 0;
+        public double armPos = 0;
+    }
+
+    static final State<Robot> idleState =
+            new State<Robot>("Idle") {
+                @Override
+                protected void periodic(StateMachine<Robot> stateMachine, Robot world) {
+                    world.motorSpeed = 0;
+                }
+            };
+
+    static final State<Robot> intakingState =
+            new State<Robot>("Intaking") {
+                @Override
+                protected void periodic(StateMachine<Robot> stateMachine, Robot world) {
+                    world.motorSpeed = -100;
+                    world.armPos = -1;
+                    if (world.hasNote) {
+                        finish();
+                    }
+                }
+            };
+
+    static final State<Robot> warmingUpState =
+            new State<Robot>("WarmingUp") {
+                @Override
+                protected void periodic(StateMachine<Robot> stateMachine, Robot world) {
+                    if (!world.hasNote) {
+                        stateMachine.requestState(idleState);
+                        return;
+                    }
+                    world.motorSpeed = 50;
+                    world.armPos = 3;
+                    finish();
+                }
+            };
+
+    static final State<Robot> shootingState =
+            new State<Robot>("Shooting") {
+                @Override
+                protected void periodic(StateMachine<Robot> stateMachine, Robot world) {
+                    world.motorSpeed = 100;
+                    if (!world.hasNote) {
+                        finish();
+                    }
+                }
+            };
+}
