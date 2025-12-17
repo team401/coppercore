931a6c73891bd4c20e81da3cc30afe2e48e1d697
aidnem <99768676+aidnem@users.noreply.github.com>
2025-12-17 12:07:27 -0500

    (#178) Clean up DriveWithJoysticks constructors, add parameter validation for deadband, improve javadocs

--- wpilib_interface/src/main/java/coppercore/wpilib_interface/DriveWithJoysticks.java
+++ wpilib_interface/src/main/java/coppercore/wpilib_interface/DriveWithJoysticks.java
@@ -12,9 +12,9 @@ import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import java.util.function.Supplier;
 
 /**
- * A command to control a drivetrain using joysticks as input.
- *
- * <p>This command squares the magnitude of the inputs for more precise low-speed control.
+ * The DriveWithJoysticks command controls a holonomic drivetrain using the joysticks or suppliers
+ * provided. It translates from the joystick to field coordinates, applies joystick deadbands, and
+ * squares joystick inputs for more precise control at low speeds.
  */
 public class DriveWithJoysticks extends Command {
     private final DriveTemplate drive;
@@ -26,10 +26,10 @@ public class DriveWithJoysticks extends Command {
     private final double joystickDeadband;
 
     /**
-     * Initialize a DriveWithJoysticks command using CommandJoysticks for input.
+     * Create a new DriveWithJoysticks command using CommandJoystick objects.
      *
-     * <p>If using a controller that does not support CommandJoystick, there is an alternate
-     * initializer that accepts suppliers.
+     * <p>An alternate constructor exists for non-CommandJoystick teams that simply takes a
+     * Supplier<Double> for each joystick input.
      *
      * @param drive The drive subsystem supplied by the robot project.
      * @param leftJoystick The left (translation/strafe) joystick.
@@ -47,37 +47,34 @@ public class DriveWithJoysticks extends Command {
             double maxLinearVelocity,
             double maxAngularVelocity,
             double joystickDeadband) {
-        this.drive = drive;
-        this.driveXSupplier = () -> leftJoystick.getX();
-        this.driveYSupplier = () -> leftJoystick.getY();
-        this.rotationSupplier = () -> rightJoystick.getX();
-
-        this.maxLinearVelocity = maxLinearVelocity;
-        this.maxAngularVelocity = maxAngularVelocity;
-        this.joystickDeadband = joystickDeadband;
-
-        addRequirements(this.drive);
+        this(
+                drive,
+                leftJoystick::getX,
+                leftJoystick::getY,
+                rightJoystick::getX,
+                maxLinearVelocity,
+                maxAngularVelocity,
+                joystickDeadband);
     }
 
     /**
-     * Initialize a DriveWithJoysticks command using suppliers for input.
+     * Create a new DriveWithJoysticks command using suppliers for joystick inputs.
      *
-     * <p>If using a controller that supports CommandJoystick, there is an alternate initializer
-     * that accepts CommandJoystick objects.
+     * <p>An alternate constructor exists for CommandJoystick teams that uses a CommandJoystick for
+     * each stick, rather than suppliers.
      *
-     * @param drive The Drive subsystem supplied by the robot project.
-     * @param driveXSupplier A double supplier supplying the X axis of the strafe. joystick. This is
-     *     the left/right drive control as viewed from the top of the joystick, NOT the X axis in
-     *     robot coordinates.
-     * @param driveYSupplier A double supplier supplying the Y axis of the strafe joystick. This is
-     *     the up/down drive control as viewed from the top of the joystick, NOT the Y axis in robot
-     *     coordinates.
-     * @param rotationSupplier A double supplier supplying the steer axis input.
-     * @param maxLinearVelocity Maximum driving velocity, which will be commanded when joystick is
-     *     fully deflected. In m/s.
-     * @param maxAngularVelocity Maximum steering velocity, which will be commanded when steer
-     *     joystick is fully deflected. In rad/s.
-     * @param joystickDeadband Deadband to apply to the joystick inputs.
+     * @param drive This is the drive subsystem supplied by the robot project
+     * @param driveXSupplier A supplier for drive translation, in the X axis of the joystick. This
+     *     becomes -y translation in field coordinates.
+     * @param driveYSupplier A supplier for drive translation, in the Y axis of the joystick. This
+     *     becomes -x translation in field coordinates.
+     * @param rotationSupplier A supplier for drive rotation, in the X axis of the joystick. This
+     *     becomes -omega in field coordinates.
+     * @param maxLinearVelocity The maximum linear velocity, in meters per second
+     * @param maxAngularVelocity The maximum angular velocity, in radians per second
+     * @param joystickDeadband The deadband of the joysticks, as a fraction (0.0 to 1.0). This value
+     *     is applied in both directions from zero (e.g. a deadband of 0.17 means that inputs from
+     *     -0.17 to 0.17 are ignored).
      */
     public DriveWithJoysticks(
             DriveTemplate drive,
@@ -97,6 +94,11 @@ public class DriveWithJoysticks extends Command {
 
         this.maxLinearVelocity = maxLinearVelocity;
         this.maxAngularVelocity = maxAngularVelocity;
+
+        if (joystickDeadband < 0.0 || 1.0 < joystickDeadband) {
+            throw new IllegalArgumentException(
+                    "Joystick deadband must be between 0 and 1 but was " + joystickDeadband);
+        }
         this.joystickDeadband = joystickDeadband;
 
         addRequirements(this.drive);
