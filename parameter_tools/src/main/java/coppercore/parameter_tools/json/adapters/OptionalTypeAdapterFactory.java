17e1f412302db6e43913f026a38dc8ceb59ccafb
Theta Back <thetaback@gmail.com>
2026-06-08 15:13:05 -0400

    (#231) fixed javadoc

--- parameter_tools/src/main/java/coppercore/parameter_tools/json/adapters/OptionalTypeAdapterFactory.java
+++ parameter_tools/src/main/java/coppercore/parameter_tools/json/adapters/OptionalTypeAdapterFactory.java
@@ -16,8 +16,8 @@ import java.util.Optional;
 
 /**
  * A Gson {@link TypeAdapterFactory} that handles serialization and deserialization of {@link
- * Optional} values. This is necessary because GSON does not support java.util.Optional, see {@link
- * https://github.com/google/gson/issues/1102}
+ * Optional} values. This is necessary because GSON does not support java.util.Optional, see <a href
+ * = "https://github.com/google/gson/issues/1102">this github issue.</a>
  *
  * <ul>
  *   <li>{@code Optional.empty()} is written as JSON {@code null}.
